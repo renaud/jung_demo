86ccaab66c255622920e596b356bae4c564eeb76
Renaud Richardet <renaud@apache.org>
2010-12-08 12:55:43 +0700

    added eigenvector centrality

--- src/test/java/ch/fhnw/jung/bc/EigenvectorCentralityDemo.java
+++ src/test/java/ch/fhnw/jung/bc/EigenvectorCentralityDemo.java
@@ -1,24 +1,27 @@
 package ch.fhnw.jung.bc;
 
-import edu.uci.ics.jung.algorithms.scoring.BetweennessCentrality;
+import edu.uci.ics.jung.algorithms.scoring.EigenvectorCentrality;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.io.MatrixFile;
 
-public class BetweennessCentralityDemo {
+public class EigenvectorCentralityDemo {
 
 	public static void main(String[] args) {
+
+		String graphFile = "adjMat_2.txt";
 
 		@SuppressWarnings({ "unchecked", "rawtypes" })
 		MatrixFile<Vertex, Edge> mf = new MatrixFile(null,
 				Factories.graphFactory, Factories.vertexFactory,
 				Factories.edgeFactory);
-		Graph<Vertex, Edge> g = mf.load("adjMat_ref.txt");
+		Graph<Vertex, Edge> g = mf.load(graphFile);
 		System.out.println("Loaded Graph:\n" + g);
-		
-		BetweennessCentrality<Vertex, Edge> bc = new BetweennessCentrality<Vertex, Edge>(
+
+		EigenvectorCentrality<Vertex, Edge> ec = new EigenvectorCentrality<Vertex, Edge>(
 				g);
 		for (Vertex v : g.getVertices()) {
-			System.out.println("BetweennessCentrality for\t" + v + "\t" + bc.getVertexScore(v));
+			System.out.println("EigenvectorCentrality for\t" + v + "\t"
+					+ ec.getVertexScore(v));
 		}
 	}
 }
